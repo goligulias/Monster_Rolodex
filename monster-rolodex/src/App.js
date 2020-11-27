f19f22dc5b17da5c34a3b4d0de55849981a5499a
Adrian Gulias Mata <adrian.gulias@outlook.com>
2020-11-26 21:32:55 -0600

    Initial commit

---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,27 @@
+import {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      string:'Hello Adrian'
+    };
+  }
+  render(){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {this.state.string}
+        </p>
+        <button onClick={()=>this.setState({string:'Hello Goli'})}>Change Text</button>
+      </header>
+    </div>
+    );
+  }
+}
+
+export default App;
