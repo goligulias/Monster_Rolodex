dbc38a1c93934fdaf021dc8df1ff7c6a829700ff
Adrian Gulias Mata <adrian.gulias@outlook.com>
2020-11-27 22:30:52 -0600

    Chapter 33

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,25 +1,32 @@
 import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component{
   constructor(){
     super();
     this.state={
-      string:'Hello Adrian'
+      monsters:[]
     };
+  }
+
+  componentDidMount(){
+	  fetch('https://jsonplaceholder.typicode.com/users')
+	  .then(response => response.json())
+	  .then(users=>this.setState({monsters:users}))
   }
   render(){
     return(
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={()=>this.setState({string:'Hello Goli'})}>Change Text</button>
-      </header>
-    </div>
+      	<div className="App">
+		<CardList name="Goli">
+			{
+				this.state.monsters.map(monster => (
+					<h1 key={monster.id}>{monster.name}</h1>
+				))
+			}
+		</CardList>
+      	</div>
     );
   }
 }
