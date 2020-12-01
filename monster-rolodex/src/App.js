d8b8b53df99844493b27d8e1d1a575409ea435be
Adrian Gulias Mata <adrian.gulias@outlook.com>
2020-12-01 00:05:06 -0600

    37

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component';
 
@@ -7,7 +6,8 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      monsters:[]
+      monsters:[],
+      searchField:''
     };
   }
 
@@ -19,6 +19,7 @@ class App extends Component{
   render(){
     return(
       	<div className="App">
+          <input type="search" placeholder="search monster" onChange={e => this.setState({searchField:e.target.value})}/>
 		    <CardList monsters={this.state.monsters}></CardList>
       	</div>
     );
